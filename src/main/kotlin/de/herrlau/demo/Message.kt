34a4468f2384b276e5351af92d1c00535b11ca05
Andi Lau <andi@herrlau.de>
2022-03-04 23:11:38 +0100

    Mod Message factor response logic into handler class

--- src/main/kotlin/de/herrlau/demo/Message.kt
+++ src/main/kotlin/de/herrlau/demo/Message.kt
@@ -1,33 +1,54 @@
 package de.herrlau.demo
 
+import de.herrlau.demo.kx.uuid
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.function.RouterFunctions.route
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse.ok
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
-@RestController
-class MessageResource(private val repository: MessageRepository) {
+@Configuration
+class MessageRoutes {
 
-    @GetMapping("/messages")
-    fun all() = repository.findAll(Sort.by("id", "text"))
+    @Bean
+    fun handler(repository: MessageRepository) = MessageHandler(repository)
 
-    @GetMapping("/messages/{id}")
-    fun one(@PathVariable id: String) = repository.findById(id)
+    @Bean
+    fun all(handler: MessageHandler) = route()
+        .GET("/messages", handler::all)
+        .build()
 
-    @PostMapping("/messages")
-    fun post(@RequestBody message: Message) {
-        repository.save(message)
-    }
+    @Bean
+    fun one(handler: MessageHandler) = route()
+        .GET("/messages/{id}", handler::one)
+        .build()
+
+    @Bean
+    fun post(handler: MessageHandler) = route()
+        .POST("/messages", handler::post)
+        .build()
+
 }
 
-interface MessageRepository : JpaRepository<Message?, String?>
+class MessageHandler(private val repository: MessageRepository) {
+
+    fun all(request: ServerRequest) = ok().body(repository.findAll(Sort.by("id", "text")))
+
+    fun one(request: ServerRequest) = ok().body(repository.findById(request.pathVariable("id")))
+
+    fun post(request: ServerRequest) =
+        ok().body(repository.save(request.body(Message::class.java).apply { id = text.uuid() }))
+}
+
+interface MessageRepository : JpaRepository<Message, String?>
 
 @Entity
 class Message(
     @Id
-    @GeneratedValue
-    val id: String?,
-    var text: String
+    var id: String?,
+    val text: String
 )
